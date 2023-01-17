4852915b3132082f04f4d9df492ad62d36d7539a
SujiSundarRaj <122840940+SujiSundarRaj@users.noreply.github.com>
2023-01-17 13:25:59 +0000

    Update profilo-pwa.js

--- profilo-pwa.js
+++ profilo-pwa.js
@@ -1,8 +1,9 @@
 window.addEventListener('load', () => {
     registerSw();
 })
-
 async function registerSw(){
+    console.log("test")
+
 if ('serviceWorker' in navigator) {
     try{
    await navigator.serviceWorker.register('https://sujisundarraj.github.io/Suji-Web-Profilo/profilo-sw.js');
